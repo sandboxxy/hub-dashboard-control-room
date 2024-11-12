3fa6c00d7c625865b20d96ecf8b37f599172e617
Zhian N. Kamvar (UMass) <zkamvar@umass.edu>
2024-11-12 15:52:41 -0800

    update app code; now getting 403 errors :/

--- app/index.js
+++ app/index.js
@@ -1,12 +1,28 @@
 module.exports = (app) => {
-  app.on(['installation_repositories.added'], async context => {
+  app.on(['installation_repositories.removed'], async context => {
     // Whenever a new repository is added, this will send a "registration"
     // repository dispatch to hubverse-org/hub-dashboard-control-room
     //
     // A workflow will then recieve this dispatch and proceed to build the
     // requested repositories
     const client_payload = {
-      "newbies": context.repositories_added,
+      "newbies": context.payload.repositories_removed.map((r) => r.full_name),
+      "new": true,
+      "message": "Bad Repository!"
+    };
+    app.log(`A new repository was removed ${client_payload.newbies}`);
+  })
+  app.on(['installation_repositories.added'], async context => {
+    // Whenever a new repository is added, this will send a "registration"
+    // repository dispatch to hubverse-org/hub-dashboard-control-room
+    //
+    // A workflow will then recieve this dispatch and proceed to build the
+    // requested repositories
+    const owner = context.payload.installation.account.login;
+    const client_payload = {
+      "newbies": context.payload.repositories_added.map((r) => {
+         return({"owner": owner, "name": r.name})
+      }),
       "new": true,
       "message": "New Repository!"
     };
@@ -16,7 +32,8 @@ module.exports = (app) => {
       "event_type": "registration",
       "client_payload": client_payload
     }
+    app.log(`A new repository was added ${client_payload.newbies}`);
     return context.octokit.repos.createDispatchEvent(dispatch);
   })
-
 }
+
